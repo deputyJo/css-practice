ab14866ba3b4562178a2d4cf1c2a455c4bf24064
deputyJo <49234178+deputyJo@users.noreply.github.com>
2024-12-24 14:14:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// const counterDisplay = document.getElementById("counter-display");
+// const increment = document.getElementById("increment");
+// const decrement = document.getElementById("decrement");
+
+// let counter = 0;
+
+// //Get the counter value from the local storage
+// const savedCounterValue = localStorage.getItem("counterValue");
+
+// //Set the value of the current counter to the
+// //local storage saved value
+// counter = savedCounterValue;
+
+// //Display the saved counter value
+// counterDisplay.textContent = savedCounterValue;
+
+
+// increment.addEventListener("click", () => {
+//     counter++;
+
+
+// });
+
+
+// decrement.addEventListener("click", () => {
+
+//     counter--;
+
+
+
+// });
+
+// function updateCounter(incrementValue) {
+//     localStorage.setItem("counterValue", counter += incrementValue);
+//     displayCounterValue(counter += incrementValue);
+// }
+
+// function displayCounterValue() {
+//     counterDisplay.textContent = counter;
+// }
+
